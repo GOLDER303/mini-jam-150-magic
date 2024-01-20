d1dcdf02e40c69f2d21984dc1307b239e5f17fad
GOLDER303 <szymonstepien@interia.pl>
2024-01-20 15:07:01 +0100

    Add player idle and run animation

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -5,6 +5,16 @@ public partial class Player : CharacterBody2D
     public const float Speed = 300.0f;
     public const float JumpVelocity = -400.0f;
 
+    private AnimationPlayer animationPlayer;
+    private Sprite2D sprite2D;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+        sprite2D = GetNode<Sprite2D>("Sprite2D");
+    }
+
     public override void _PhysicsProcess(double delta)
     {
         float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
@@ -13,6 +23,7 @@ public partial class Player : CharacterBody2D
 
         if (!IsOnFloor())
         {
+            animationPlayer.Stop();
             velocity.Y += gravity * (float)delta;
         }
 
@@ -26,10 +37,16 @@ public partial class Player : CharacterBody2D
         if (direction != 0)
         {
             velocity.X = direction * Speed;
+
+            sprite2D.FlipH = velocity.X < 0;
+
+            animationPlayer.Play("run");
         }
         else
         {
             velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+
+            animationPlayer.Play("idle");
         }
 
         Velocity = velocity;
