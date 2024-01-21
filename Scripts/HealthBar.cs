a9f2c00f2d2f1d979462daf52b5d1df8c92389cb
GOLDER303 <szymonstepien@interia.pl>
2024-01-21 15:48:40 +0100

    Add health bar script

--- Scripts/HealthBar.cs
+++ Scripts/HealthBar.cs
@@ -1,6 +1,42 @@
+using System.Collections.Generic;
+using System.Linq;
 using Godot;
 
 public partial class HealthBar : HBoxContainer
 {
-    public void TakeDamage(int damageAmount) { }
+    private List<ColorRect> healthRects = new();
+    private int currentHealth;
+    private int maxHealth;
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        Node[] healthRectsNodes = GetChildren().Where(ch => ch is ColorRect).ToArray();
+
+        foreach (var healthRectNode in healthRectsNodes)
+        {
+            healthRects.Add((ColorRect)healthRectNode);
+        }
+
+        maxHealth = healthRects.Count;
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damageAmount)
+    {
+        currentHealth -= damageAmount;
+
+        GD.Print(currentHealth);
+
+        for (int i = currentHealth; i < maxHealth; i++)
+        {
+            healthRects[i].Hide();
+        }
+
+        if (currentHealth <= 0)
+        {
+            GD.Print($"Death {Owner.Name}");
+        }
+    }
 }
